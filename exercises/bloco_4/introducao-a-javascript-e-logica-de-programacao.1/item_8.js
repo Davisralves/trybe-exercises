1f1be338037ccc28028393161965846bcf1d5a39
Davisralves <davialves324@gmail.com>
2021-06-23 18:38:45 -0300

    adicionando item_8.js

---
+++ exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao.1/item_8.js
@@ -0,0 +1,11 @@
+num1 = 8;
+num2 = 3;
+num3 = 1;
+if(num1 % 2 == 0 ||num2 % 2 == 0 ||num3 % 2 == 0){
+    console.log("true")
+    return true;
+}
+else{
+    console.log("false")
+    return false;
+}
