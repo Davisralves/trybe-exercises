7fcb0765c25cc6bbddce2e10f502a6f2444f5bd2
Davisralves <davialves324@gmail.com>
2021-06-23 15:05:10 -0300

    Adicionando exercicio dia 4

---
+++ exercises/bloco_4/dia_1/exercicio.js
@@ -0,0 +1,12 @@
+const name = "Davi";
+const birthCity = "Recife";
+let birthYear = 1997;
+console.log(name);
+console.log(birthCity);
+console.log(birthYear);
+birthYear = 2030;
+console.log(birthYear);
+birthCity = "jampa";
+console.log(birthCity);
+
+
