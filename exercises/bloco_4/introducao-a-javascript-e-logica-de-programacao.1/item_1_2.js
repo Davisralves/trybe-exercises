29d1c29c9cf84e5a4b058cac2bcb14862ba75f44
Davisralves <davialves324@gmail.com>
2021-06-30 16:27:14 -0300

    adicinando arqyuvis bloco 4

--- exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao.1/item_1_2.js
+++ exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao.1/item_1_2.js
@@ -1,18 +1,34 @@
+function sum(a,b){
+    return a+b;
+}
+function sub(a,b){
+    return a-b;
+}
+function mult(a,b){
+    return a*b;
+}
+function divisao(a,b){
+    return a/b;
+}
+function modulo(a,b){
+    return a%b;
+}
 let a = 2;
 let b = 4;
 
-let adicao = a + b;
-let subtracao = a - b;
-let mutiplicacao = a * b;
-let divisao = a/b;
-let modulo = a % b;
+//let adicao = a + b;
+//let subtracao = a - b;
+//let mutiplicacao = a * b;
+//let divisao = a/b;
+//let modulo = a % b;
 
-if (a > b){
-    console.log("a é maior");
-}
-else if(a=b){
-    console.log("a=b")
-}
-else {
-    console.log("b é maior")
-}
+//if (a > b){
+//    console.log("a é maior");
+//}
+//else if(a=b){
+//    console.log("a=b")
+//}
+//else {
+  //  console.log("b é maior")
+//}
+console.log(modulo(a,b));
--- exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao.2/teste.js
+++ exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao.2/teste.js
@@ -1,4 +1,1 @@
-lista = [1,2,3,4,5];
-console.log(lista);
-lista = lista * 2;
-console.log(lista);
+let string = " blabla 10";
