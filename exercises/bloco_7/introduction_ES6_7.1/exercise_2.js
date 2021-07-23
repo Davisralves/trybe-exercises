cff326a23e782eb6bbd17541e13ac4f63cd25282
Davisralves <davialves324@gmail.com>
2021-07-23 12:09:26 -0300

    adicionando exercicio 2 com funçao recursiva

--- exercises/bloco_7/introduction_ES6_7.1/exercise_2.js
+++ exercises/bloco_7/introduction_ES6_7.1/exercise_2.js
@@ -1,15 +1,34 @@
- /*// 1. Crie uma função que receba um número e retorne seu fatorial.
+/*// 1. Crie uma função que receba um número e retorne seu fatorial.
 const factorial = num => {
-  for (let index = num - 1; index > 0; index -= 1) { testar for (let index of num)
+  for (let index = num - 1; index > 0; index -= 1) { //testar for (let index of num)
     num *= index;
   }
   return num;
 }
 console.log(factorial(10)); */
+/*
+function factorial(x) 
+{ 
+
+  if (x === 0)
+ {
+    return 1;
+ }
+  return x * factorial(x-1);
+         
+}
+
+// fazendo fatorial em uma linha :
+let factorial = x => x === 0 ? 1 : x * factorial(x-1)
+console.log(factorial(5));*/
+/*
+let fatorial = num => { num > 2 ? num *= num-1: num;}
+console.log(fatorial(10)); */
+
 
 // 2. Crie uma função que receba uma frase e retorne qual a maior palavra.
 
-string = "Antônio foi no banheiro e não sabemos o que aconteceu"
+/*string = "Antônio foi no banheiro e não sabemos o que aconteceu"
 const maiorPalavra = string => {
   let array = string.split(" ");
   let maiorPalavra = "";
@@ -18,4 +37,6 @@ const maiorPalavra = string => {
   }
   return maiorPalavra;
 }
-console.log(maiorPalavra(string));
+console.log(maiorPalavra(string)); */
+
+// 3.
