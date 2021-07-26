022d1614461b5d11ed928edcf3dbaa9f73e71c0d
Davisralves <davialves324@gmail.com>
2021-07-26 19:21:15 -0300

    adicionando exercicio 1

---
+++ exercises/bloco_7/testes_unitarios_7.3/parte_2/testes.js
@@ -0,0 +1,8 @@
+let array = [1,2];
+array[0] = array[0] + 1;
+console.log(array);
+let array 
+for(let index = 0; index <= myArray.length - 1; index += 1) {
+  array[index] = array[index] + 1;
+};
+return array;
