99e4f18e1524e65674eec085de6f441c5f95053d
Davisralves <davialves324@gmail.com>
2021-08-19 16:20:33 -0300

    finalizando primeira parte exercicio 2

--- exercises/bloco_10/jest_assincronos/testes/script.test.js
+++ exercises/bloco_10/jest_assincronos/testes/script.test.js
@@ -1,4 +1,4 @@
-const {asyncSum, helloWord, uppercase} =  require('../script.js');
+const {asyncSum, helloWord, uppercase, findUserById, getUserName,} =  require('../script.js');
 
 test('Testando asyncSum, soma 5 mais 10', (done) => {
   asyncSum(5, 10, (result) => {
@@ -24,4 +24,27 @@ test('Testando uppercase, Deixa texto em maiusculo', (done) => {
       done(erro);
     }
   });
+});
+
+describe('Fazendo testes dos itens relacionados a função findUserById', () => {
+  test('Testando se findUserById retorna corretamente ao se dar o id correto', (done) => {
+    findUserById(1).then((result) => {
+      try {
+        expect(result).toEqual({id: 1, name: 'Mark'});
+        done();
+      } catch (erro) {
+        done(erro);
+      }
+    });
+  });
+  test('Testar Se o user Retorna corretamente a um erro caso o usario não seja encontrado', () => {
+    findUserById(1).then((result) => {
+      try {
+        expect(result).toEqual('Erro ! Não deveria funcionar');
+        done(result);
+      } catch(erro) {
+        done(erro);
+      }
+    });
+  })
 });
