4ee197bdaecd533ba5c340749c96e227db2a27dc
Davisralves <davialves324@gmail.com>
2021-09-06 17:43:55 -0300

    adicionando validação proptypes e alguns estilos css

--- exercises/bloco_12/formularios_react/formulario_react/src/App.js
+++ exercises/bloco_12/formularios_react/formulario_react/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import InputPadrao from './InputPadrao'
+import Estado from './Estado'
 
 class App extends React.Component {
   constructor () {
@@ -10,6 +11,7 @@ class App extends React.Component {
       cpf: '',
       endereço: '',
       cidade: '',
+      estado: '',
       logadouro: '',
       curriculo: '',
       cargo: '',
@@ -27,45 +29,44 @@ class App extends React.Component {
   render () {
     return (
       <form>
-      <fieldset>
-      <InputPadrao type="text" maxLength="40" placeholder="Nome" className="dados" id="nome" funcao={this.handleClick}/>
-      <InputPadrao type="email" maxLength="50" placeholder="E-mail" className="dados" id="email" funcao={this.handleClick}/>
-        <input type="text" maxLength="11" placeholder="Cpf" className="dados" ></input>
-        <input type="text" maxLength="200" placeholder="Endereço" className="dados" ></input>
-        <input type="text" maxLength="28" placeholder="Cidade" className="dados" ></input>
-        <p>
-          Estado:
-          <select>
-          </select>
+        <fieldset className="primeiraParte">
+          <InputPadrao type="text" maxLength="40" placeholder="Nome" className="form-control" id="nome" funcao={this.handleClick}/>
+          <InputPadrao type="email" maxLength="50" placeholder="E-mail" className="form-control" id="email" funcao={this.handleClick}/>
+          <InputPadrao type="text" maxLength="11" placeholder="Cpf" className="form-control" id="cpf" funcao={this.handleClick}/>
+          <InputPadrao type="text" maxLength="200" placeholder="Endereço" className="form-control" id="endereço" funcao={this.handleClick}/>
+          <InputPadrao input type="text" maxLength="28" placeholder="Cidade" className="form-control" id="cidade" funcao={this.handleClick}/>
+            <p>
+              Estado:
+              <Estado id="estado" funcao={this.handleClick} />
+            </p>
+          <div>
+              <span>logadouro:</span>
+                <label forhtml="casa">
+                  <span>Casa</span>
+                  <input type="radio" id="casa" name="logadouro"></input>
+                </label>
+                <label forhtml="apartamento">
+                  <span>Apartamento</span>
+                  <input type="radio" id="apartamento" name="logadouro"></input>
+                </label>
+        </div>
+      </fieldset>
+        <fieldset>
+          <p>Resumo do curriculo:</p>
+          <textarea name="Resumo" id="curriculo" cols="100" rows="10" maxLength="1000" ></textarea>
+          <p>Cargo:
+          <input type="text" maxLength="40" ></input>
         </p>
-        <div>
-          <span>logadouro:</span>
-            <label forhtml="casa">
-              <span>Casa</span>
-              <input type="radio" id="casa" name="logadouro"></input>
-            </label>
-            <label forhtml="apartamento">
-              <span>Apartamento</span>
-              <input type="radio" id="apartamento" name="logadouro"></input>
-            </label>
-    </div>
-      </fieldset>
-      <fieldset>
-        <p>Resumo do curriculo:</p>
-        <textarea name="Resumo" id="curriculo" cols="100" rows="10" maxLength="1000" ></textarea>
-        <p>Cargo:
-        <input type="text" maxLength="40" ></input>
-      </p>
-      <p>Descrição do cargo:
-        <input type="text" maxLength="500" id="descricao de cargo" ></input>
-      </p>
-      <p>
-        Data de inicio:
-        <input type="date" ></input>
-      </p>
-      <button id="button" type="submit">Enviar</button>
+        <p>Descrição do cargo:
+          <input type="text" maxLength="500" id="descricao de cargo" ></input>
+        </p>
+        <p>
+          Data de inicio:
+          <input type="date" ></input>
+        </p>
+        <button id="button" type="submit">Enviar</button>
 
-      </fieldset>
+        </fieldset>
     </form>
     )
   }
--- exercises/bloco_12/formularios_react/formulario_react/src/InputPadrao.js
+++ exercises/bloco_12/formularios_react/formulario_react/src/InputPadrao.js
@@ -1,13 +1,30 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 class InputPadrao extends React.Component {
   render () {
-    // eslint-disable-next-line react/prop-types
     const { funcao, type, maxLength, placeholder, className, id } = this.props
     return (
-      <input type={type} maxLength={maxLength} placeholder={placeholder} className={className} id={id} onChange={funcao}></input>
+      <div className="input-group mb-3">
+  <span className="input-group-text" id="basic-addon1">{placeholder}:</span>
+  <input type={type} maxLength={maxLength} className={className} id={id} onChange={funcao}></input>
+</div>
     )
   }
 }
 
+InputPadrao.propTypes = {
+  funcao: PropTypes.func.isRequired,
+  type: PropTypes.string.isRequired,
+  maxLength: PropTypes.string,
+  placeholder: PropTypes.string,
+  className: PropTypes.string,
+  id: PropTypes.string.isRequired
+}
+
+InputPadrao.defaultProps = {
+  maxLength: '',
+  placeholder: '',
+  className: ''
+}
 export default InputPadrao
