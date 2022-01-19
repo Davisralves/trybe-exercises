9f587849dc7121c2fce4c1b62a407f11bdd9b598
Davisralves <davialves324@gmail.com>
2022-01-19 18:59:35 -0300

    finalizando exercicio incialcial

---
+++ exercises/bloco_23/model_example/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const Author = require("./models/Author");
+
+const Books = require("./models/books");
+
+const app = express();
+
+app.get("/authors", async (_req, res) => {
+	const authors = await Author.getAll();
+
+	res.status(200).json(authors);
+});
+
+app.get("/books", async (_req, res) => {
+	const books = await Books.getAll();
+	res.status(200).json(books);
+});
+
+app.get("/books/:id", async (req, res) => {
+	const { id } = req.query;
+	const books = await Books.getByAuthorId(id);
+  if (!books) return res.status(404).json({ message: 'Not found' });
+	res.status(200).json(books);
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+	console.log(`Ouvindo a porta ${PORT}`);
+});
